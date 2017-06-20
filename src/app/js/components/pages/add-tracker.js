d1e6a1a876a27b8b6a9d308002e946b71bc00de1
Phil Gibbins <scoobster17@hotmail.co.uk>
2017-06-20 10:39:53 +0100

    Fixed radios on add tracker type, ensure form submits correctly again

--- src/app/js/components/pages/add-tracker.js
+++ src/app/js/components/pages/add-tracker.js
@@ -3,14 +3,29 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class AddTrackerPage extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            form: {
+                selectedTrackerType: 'people'
+            }
+        };
+
+        this.handleTrackerTypeChange = this.handleTrackerTypeChange.bind(this);
+    }
+
     render() {
+        const { user } = this.props;
+        const { selectedTrackerType } = this.state.form;
         return (
             <main>
                 <form id="add-tracker-form">
                     <h1>Add a score tracker</h1>
                     <p>Create a tracking sheet for your scores here.</p>
                     <fieldset>
-                        <input type="hidden" name="creator" value="thisUsersId" />
+                        <input type="hidden" name="creator" value={ user && user.id } />
                     </fieldset>
                     <fieldset>
                         <h2>Tracker details</h2>
@@ -21,18 +36,18 @@ class AddTrackerPage extends React.Component {
                         <fieldset>
                             <span>Tracker type:</span>
                             <label htmlFor="people">People</label>
-                            <input type="radio" name="type" id="people" value="People" checked="checked" />
+                            <input type="radio" name="type" id="people" value="people" checked={ this.state.form.selectedTrackerType === 'people' } onChange={ this.handleTrackerTypeChange } />
                             <label htmlFor="team">Team</label>
-                            <input type="radio" name="type" id="team" value="Team" />
-                            <fieldset className="competitorInformation">
+                            <input type="radio" name="type" id="team" value="team" checked={ this.state.form.selectedTrackerType === 'team' } onChange={ this.handleTrackerTypeChange } />
+                            <fieldset hidden={ selectedTrackerType !== 'people' }>
                                 <label htmlFor="competitors">Competitors:</label>
                                 <input type="text" id="competitors" name="competitors" />
                             </fieldset>
-                            <fieldset className="competitorInformation">
+                            <fieldset hidden={ selectedTrackerType !== 'people' }>
                                 <h3>Administrators</h3>
                             </fieldset>
-                            <fieldset className="teamInformation">
-                                Sorry, this feature is not yet supported!
+                            <fieldset hidden={ selectedTrackerType !== 'team' }>
+                                COMING SOON! Sorry, this feature is not yet supported.
                             </fieldset>
                         </fieldset>
                     </fieldset>
@@ -40,6 +55,14 @@ class AddTrackerPage extends React.Component {
                 </form>
             </main>
         )
+    }
+
+    handleTrackerTypeChange(event) {
+        this.setState({
+            form: {
+                selectedTrackerType: event.target.value.toLowerCase()
+            }
+        });
     }
 
     componentWillMount() {
@@ -72,10 +95,12 @@ class AddTrackerPage extends React.Component {
                 formDataObj[inputs[i].name] = inputs[i].value;
             }
 
-            formDataObj["urlText"] = formDataObj.name.toLowerCase().replace(/ /g, '-');
+            formDataObj['type'] = this.state.form.selectedTrackerType;
+            formDataObj['urlText'] = formDataObj.name.toLowerCase().replace(/ /g, '-');
 
             // send data via AJAX
             request.send(JSON.stringify(formDataObj));
+
             return false;
 
         });
